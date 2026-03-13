7456c9a720dc492649198ef2e7d96e8b0c6b689d
Daniel Jorge <danjorge@gmail.com>
2026-03-13 19:27:26 -0300

    fix: import FinanceModule in OrdersModule for payment functionality

--- backend/src/modules/orders/orders.module.ts
+++ backend/src/modules/orders/orders.module.ts
@@ -3,9 +3,10 @@ import { OrdersController } from './orders/orders.controller';
 import { OrdersService } from './orders/orders.service';
 import { PrismaModule } from '../../common/db/prisma.module';
 import { IntegrationsModule } from '../integrations/integrations.module';
+import { FinanceModule } from '../finance/finance.module';
 
 @Module({
-  imports: [PrismaModule, IntegrationsModule],
+  imports: [PrismaModule, IntegrationsModule, FinanceModule],
   controllers: [OrdersController],
   providers: [OrdersService],
   exports: [OrdersService],
